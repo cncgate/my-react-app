f866b9b5eaff81ad36973b5bd61eba4a6ecb5d4f
Dmitrijs Smirnovs <dmitrijs.smirnovs@sapiens.com>
2020-10-07 12:22:32 +0300

    Component complete

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 const App = () => {
     return (
-    <div className="ui container comments">
-        <CommentDetail />
-        <CommentDetail />
-        <CommentDetail />
-        <CommentDetail />
-    </div>
+        <div className="ui container comments">
+            <CommentDetail
+                author="Sam"
+                timeAgo="Today at 4:45PM"
+                content="Briliant article!"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="Alex"
+                timeAgo="Today at 2:00PM"
+                content="Well done Dmitry!"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="Jane"
+                timeAgo="Today at 1:45PM"
+                content="I should disagree..."
+                avatar={faker.image.avatar()}
+            />
+        </div>
     );
 };
 
