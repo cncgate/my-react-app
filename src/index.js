aad8e60062a92af9e9dc3621452f2fe969bbb012
Dmitrijs Smirnovs <dmitrijs.smirnovs@sapiens.com>
2020-10-12 09:21:39 +0300

    Empty project

--- src/index.js
+++ src/index.js
@@ -1,39 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CommentDetail from './CommentDetail';
-import faker from 'faker';
-import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
-        <div className="ui container comments">
-            <ApprovalCard>
-            <CommentDetail
-                author="Sam"
-                timeAgo="Today at 4:45PM"
-                content="Briliant article!"
-                avatar={faker.image.avatar()}
-            />
-            </ApprovalCard>
-
-            <ApprovalCard>
-            <CommentDetail
-                author="Alex"
-                timeAgo="Today at 2:00PM"
-                content="Well done Dmitry!"
-                avatar={faker.image.avatar()}
-            />
-            </ApprovalCard>
-            
-            <ApprovalCard>
-            <CommentDetail
-                author="Jane"
-                timeAgo="Today at 1:45PM"
-                content="I should disagree..."
-                avatar={faker.image.avatar()}
-            />
-            </ApprovalCard>
-        </div>
+        <div></div>
     );
 };
 
