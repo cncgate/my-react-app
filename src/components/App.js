731c70f1c617c2ba6fa9d172511ab9d95e4c7126
Dmitrijs Smirnovs <dmitrijs.smirnovs@sapiens.com>
2020-10-12 12:29:32 +0300

    95 Api requests with axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,32 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
+import Secrets from "./Secrets";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const headerValue = `Client-ID ${Secrets}`;
+
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: headerValue
+      }
+    });
+
+    this.setState({ images: response.data.results });
+  };
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        Found {this.state.images.length} images
+      </div>
+    );
+  }
+}
 
 export default App;
