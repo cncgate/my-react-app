18794eef11e0411d434115ac978dd8b5e0b6efbe
Dmitrijs Smirnovs <dmitrijs.smirnovs@sapiens.com>
2020-11-23 09:15:32 +0200

    180 Event bubling, useRef

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    document.body.addEventListener("click", (event) => {
+      if (ref.current.contains(event.target)) {
+        return;
+      }
+
+      setOpen(false);
+    });
+  }, []);
 
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) {
@@ -20,16 +31,18 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   });
 
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
         <div
           onClick={() => setOpen(!open)}
-          className={`ui selection dropdown ${ open ? 'visible active' : ''}`}        
-          >
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className={`menu ${open ? 'visible transition' : ''}`}>{renderedOptions}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderedOptions}
+          </div>
         </div>
       </div>
     </div>
