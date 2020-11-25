f1b153da1da5a7052e71f2c083a65015b396b9a9
Dmitrijs Smirnovs <dmitrijs.smirnovs@sapiens.com>
2020-11-25 09:01:44 +0200

    185 Init convert component

---
+++ src/components/Translate.js
@@ -0,0 +1,41 @@
+import React, { useDebugValue, useState } from "react";
+import Dropdown from "./Dropdown";
+
+const options = [
+  {
+    label: "Afrikaans",
+    value: "af",
+  },
+  {
+    label: "Arabic",
+    value: "ar",
+  },
+  {
+    label: "Hindi",
+    value: "hi",
+  },
+];
+
+const Translate = () => {
+  const [language, setLanguage] = useState(options[0]);
+  const [text, setTetxt] = useState("");
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Text</label>
+          <input value={text} onChange={(e) => setTetxt(e.target.value)} />
+        </div>
+      </div>
+      <Dropdown
+        label="Select a Language"
+        selected={language}
+        onSelectedChange={setLanguage}
+        options={options}
+      />
+    </div>
+  );
+};
+
+export default Translate;
