108a30f9ed1995c4dbc56b55a773d0c33721fb24
Dmitrijs Smirnovs <dmitrijs.smirnovs@sapiens.com>
2020-11-26 08:37:58 +0200

    189 Convert component intoduced

---
+++ src/components/Convert.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+
+const Convert = ({ language, text }) => {
+  
+    useEffect(() => {
+    console.log("dd");
+  }, [language, text]);
+
+  return <div/>
+};
+
+export default Convert;
