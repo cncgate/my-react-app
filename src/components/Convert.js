1e041522614d1de95c3824aad8e453ad93299c26
Dmitrijs Smirnovs <dmitrijs.smirnovs@sapiens.com>
2020-11-26 09:01:17 +0200

    191 Update convert component

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,12 +1,36 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import GoogleApiKey from "./GoogleApiKey";
 
 const Convert = ({ language, text }) => {
-  
-    useEffect(() => {
-    console.log("dd");
+  const [translated, setTranslated] = useState("");
+
+  useEffect(() => {
+    const url = "https://translation.googleapis.com/language/translate/v2";
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        url,
+        {},
+        {
+          params: {
+            q: text,
+            target: language.value,
+            key: GoogleApiKey,
+          },
+        }
+      );
+
+      setTranslated(data.data.translations[0].translatedText);
+    };
+    doTranslation();
   }, [language, text]);
 
-  return <div/>
+  return (
+    <div>
+      <h1 className="ui header"></h1>
+      {translated}
+    </div>
+  );
 };
 
 export default Convert;
